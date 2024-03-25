3e938db6b2ace6b1d91e9ce9a2ca54ebc73b4189
Guilherme Silva <guilherme.fsilva@skyone.solutions>
2024-03-25 20:35:43 -0300

    remove unused Func

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/joho/godotenv"
 
-	"oh-my-chat/src/notion"
-	"oh-my-chat/src/service"
 	"oh-my-chat/src/telegram"
 )
 
@@ -18,21 +16,4 @@ func main() {
 	}
 
 	telegram.NewEngine().Chating(30)
-}
-
-func Run() {
-
-	bus := service.NewBus()
-	bus.SetHandler(
-		"notion_inspect_study_road_map",
-		notion.NewStudyInspectHandler(&notion.SketchRepo{}, bus),
-	)
-	bus.SetHandler("notion_study_pendency", telegram.NewTelegramPendencyHandler())
-
-	bus.Consume()
-
-	notion.StudyInspect(bus)
-
-	select {}
-
 }
