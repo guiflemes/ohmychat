42057a208dd7dbf3134b30c0ddfb00f1cc4383c6
Guilherme Silva <guilherme.1995lemes@gmail.com>
2025-07-04 16:20:52 -0300

    delete schemas but keep models by now

--- src/config/config.go
+++ src/config/config.go
@@ -1,19 +1,5 @@
 package config
 
-type Worker struct {
-	Enabled bool
-	Number  int
-}
-
-type Api struct {
-	Enabled bool
-	Port    int
-}
-
-const (
-	MessageOmitted string = "omitted"
-)
-
 type Provider string
 
 const (
@@ -25,14 +11,6 @@ type Connector struct {
 	Provider Provider
 }
 
-type ChatDatabase struct {
-	Kind     string
-	Settings map[string]any
-}
-
 type OhMyChatConfig struct {
-	Api          Api
-	Worker       Worker
-	Connector    Connector
-	ChatDatabase ChatDatabase
+	Connector Connector
 }
